45298585b907e8277e5d2e133f90bf1d249c291d
Sovan Patnayak <sovanpatnayak@gmail.com>
2022-07-07 01:15:20 -0400

    Implemented functionality to calculate age given DOB instead of year

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"os"
-	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
@@ -31,23 +31,50 @@ func main() {
 		log.Fatalf("Error loading .env file")
 	}
 
-	// getting env variables SLACK_BOT_TOKEN and SLACK_APP_TOKEN
+	// creating new slacker bot and getting env variables SLACK_BOT_TOKEN and SLACK_APP_TOKEN
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
-	// print the CommandEvents
+	// printing all commands that take place to console (optional)
 	go printCommandEvents(bot.CommandEvents())
 
-	bot.Command("my yob is <year>", &slacker.CommandDefinition{
-		Description: "yob calculator",
-		Example:     "my yob is 2020",
+	// when bot sees this command, it will handle as follows
+	bot.Command("my dob is <BirthDate>", &slacker.CommandDefinition{
+		Description: "age calculator",
+		Example:     "my dob is 2000-07-11",
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
-			year := request.Param("year")
-			yob, err := strconv.Atoi(year)
+
+			// store inputted birthdate
+			birthdate := request.Param("BirthDate")
+
+			// parse and format birthdate
+			b_date, err := time.Parse("2006-01-02", birthdate)
 			if err != nil {
-				println("error")
+				println("error parsing birthdate")
 			}
-			age := 2022 - yob
-			r := fmt.Sprintf("age is %d", age)
+			b_year, b_month, b_day := b_date.Date()
+			b_date = time.Date(b_year, b_month, b_day, 0, 0, 0, 0, time.UTC)
+
+			// store and format current date
+			c_date := time.Now()
+			c_year, c_month, c_day := c_date.Date()
+			c_date = time.Date(c_year, c_month, c_day, 0, 0, 0, 0, time.UTC)
+
+			// returns true if current date is present before birthdate
+			if c_date.Before(b_date) {
+				return
+			}
+
+			// calculate age
+			age := c_year - b_year
+
+			// calculate anniversary
+			anniversary := b_date.AddDate(age, 0, 0)
+			if anniversary.After(c_date) {
+				age--
+			}
+
+			// output age
+			r := fmt.Sprintf("your age is %d", age)
 			response.Reply(r)
 		},
 	})
